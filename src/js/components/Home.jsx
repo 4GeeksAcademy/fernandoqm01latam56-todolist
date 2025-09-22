27b2307ff25c425f62281a7c9a6216e288e01de1
fernandoqm01 <fernandoquesadamora@gmail.com>
2025-09-22 23:33:30 +0000

    todo list

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,57 @@
-import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
+
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	const [newTodo, setNewTodo] = useState("");
+	const [list, setList] = useState(["Lavar la ropa ", "Limpiar la casa", "Hacer la cena"]);
+
+	const handelClick = () => {
+		if (newTodo.length != 0) {
+			setList([...list, newTodo]);
+		}
+		else {
+			alert("Introduce una tarea")
+		}
+	}
+
+	const handleChange = (event) => {
+		setNewTodo(event.target.value);
+	}
+
+	const deletTarea = (indice) => {
+		const listaNueva = list.filter((Todo, i) => i !== indice)
+		setList(listaNueva);
+	}
+
+	return (
+		<div className="text-center p-2">
+
+			<h1 className="text-center mt-5">Todo List React</h1>
+			<div className="container p-2 mt-2">
+				<div className="d-flex" >
+
+					<input type="text" className="form-control" onChange={handleChange} />
+
+					<button className="btn btn-light" onClick={handelClick}>Agregar Tarea</button>
+
+				</div>
+				<div>
+					<ul className="list-group p2">
+						{list.map((Todo, indice) => {
+							return (
+								<li className="list-group-item p-1 list-group-item d-flex justify-content-between align-items-center" >
+									{Todo} <button className="btn btn-light" onClick={() => deletTarea(indice)}>X</button>
+								</li>
+							);
+						})
+						}
+					</ul>
+				</div>
+			</div>
+
 		</div>
 	);
 };
